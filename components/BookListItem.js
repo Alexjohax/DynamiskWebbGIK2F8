a000cabaf565a792f526e1f969b2efbafa73815c
Alejohax <alexander.johanssonn@gmail.com>
2022-11-09 17:21:18 +0100

    bild i sök

--- components/BookListItem.js
+++ components/BookListItem.js
@@ -1,7 +1,8 @@
 const BookListItem = (book) => {
   let html = `
-    <li class="book-list__item mb-2 mx-2 last:mb-0 p-3 text-indigo-800 last:border-b-0 border-b border-indigo-500 cursor-pointer">
-    ${book.author} - ${book.title}
+    <li class="book-list__item mb-2 mx-2 last:mb-0 p-3 text-indigo-800 
+    last:border-b-0 border-b border-indigo-500 cursor-pointer flex justify-between items-center">
+    <p>${book.author} - ${book.title}</p><img class="justify-self-end" src=${book.coverImage} alt=${book.title} width="60" height="120" />
     </li>
     `;
 
